97170ae79503e2643ce5e92d1124de403fbd8bc7
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-05-31 11:21:18 -0700

    add images and text
    Co-authored-by: Frank Timmons <franktimmonspdx@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){  
+  jQuery("h1").click(function() {
+    alert("This is a heading.");
+  });
+  
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
+  
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
+});
